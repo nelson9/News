c3b8af71b22357653a5eb92717a0e14edaf632e5
nelson9 <nferguson@macmillan.org.uk>
2016-09-21 17:24:21 +0100

    init

--- Data/NewsContext.cs
+++ Data/NewsContext.cs
@@ -17,6 +17,7 @@ namespace Data
 
         public DbSet<Article> Articles { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ Data/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.GenericRepo;
+using NewsApp.Models;
+
+namespace Data.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        
+    }
+
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public NewsContext NewsContext
+        {
+            get
+            {
+                return Context as NewsContext;
+            }
+        }
+
+        public UserRepository(NewsContext context) : base(context) { }
+    }
+}
